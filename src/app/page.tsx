99516d07ed50b7df9fee0ceadafa333a9b4ab4f7
Yong Lee <77824603+YongLeeCode@users.noreply.github.com>
2024-10-15 15:26:18 +0900

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
         />
         <ol>
           <li>
-            Get started by editing <code>src/app/page.tsx</code>.
+            화이팅Get started by editing <code>src/app/page.tsx</code>.
           </li>
           <li>Save and see your changes instantly.</li>
         </ol>
